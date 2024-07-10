286d996660ed756a5dd9be0018364cf6873d82c4
Nikita-zach <akhmetshynm@gmail.com>
2024-07-10 14:33:24 +0200

    Rewrited task 3

--- Lesson 08.07/Homework 08.07.py
+++ Lesson 08.07/Homework 08.07.py
@@ -8,14 +8,14 @@ else:
 
 #Task 2
 num=input("Enter 6-figure number:")
-palindrome=list(map(int,num))
-pal1=polindrome[0:3]
-pal2=polindrome[-1:-4:-1]
-res="This is a palindrome" if pal1==pal2 else "This is not palindrome"
+res="This is a palindrome" if num==num[::-1] else "This is not palindrome"
 print(res)
 
 #Task 3
-RAD = 4
+R=16
 x,y = int(input("Enter x:")),int(input("Enter y:"))
-result = "Point is in a circle" if x<=RAD and y<=RAD else "Point isn't in a circle"
-print(result)
+PROV=x**2+y**2
+if PROV<R:
+    print("Point is in a circle")
+else:
+    print("Point isn't in a circle")
