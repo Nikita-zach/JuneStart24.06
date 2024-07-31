a9015432c3af72d06c6d47f4ecccea0795e41cef
Nikita-zach <akhmetshynm@gmail.com>
2024-07-31 14:11:23 +0200

    fixed .readlines problem

--- Base/Lesson 29.07/Homework 29.07.py
+++ Base/Lesson 29.07/Homework 29.07.py
@@ -1,7 +1,3 @@
-with open("hw_10_test.txt", "r", encoding="UTF-8") as file:
-    lines = file.readlines()
-
-
 def data_in_line(line):
     users_data = line.split()
     amount = float(users_data[-2].strip("$"))
@@ -16,45 +12,53 @@ def format_two(diction):
 
 def exp_by_cat(lines):
     by_cat = {}
-    for line in lines:
-        amount, categ, _ = data_in_line(line)
-        if categ in by_cat:
-            by_cat[categ] += amount
-        else:
-            by_cat[categ] = amount
+    with open("hw_10_test.txt","r", encoding="UTF-8") as file:
+        for line in file:
+            amount, categ, _ = data_in_line(line)
+            if categ in by_cat:
+                by_cat[categ] += amount
+            else:
+                by_cat[categ] = amount
+
 
     return format_two(by_cat)
 
 
 def exp_by_users(lines):
     by_user = {}
-    for line in lines:
-        amount, _, name = data_in_line(line)
-        if name in by_user:
-            by_user[name] += amount
-        else:
-            by_user[name] = amount
+    with open("hw_10_test.txt","r", encoding="UTF-8") as file:
+        for line in file:
+            amount, _, name = data_in_line(line)
+            if name in by_user:
+                by_user[name] += amount
+            else:
+                by_user[name] = amount
 
     return format_two(by_user)
 
 
+file = open("hw_10_test.txt", "r", encoding="UTF-8")
+
+
 def exp_by_user(lines):
     user_exp = {}
-    for line in lines:
-        amount, categ, name = data_in_line(line)
-        if user_name == name:
-            if categ in user_exp:
-                user_exp[categ] += amount
-            else:
-                user_exp[categ] = amount
+    with open("hw_10_test.txt","r", encoding="UTF-8") as file:
+        for line in file:
+            amount, categ, name = data_in_line(line)
+            if user_name == name:
+                if categ in user_exp:
+                    user_exp[categ] += amount
+                else:
+                    user_exp[categ] = amount
+
 
     return format_two(user_exp)
 
 
 user_name = input("Enter name of a user>>>")
-res = exp_by_cat(lines)
-res1 = exp_by_users(lines)
-res2 = exp_by_user(lines)
+res = exp_by_cat(file)
+res1 = exp_by_users(file)
+res2 = exp_by_user(file)
 print("\nExpenses by category:")
 print(res)
 print("\nExpenses by user:")
